41630fa079af1e76280dba966020b85b24d8c750
Pavel <Zamerchukpavel95@bk.ru>
2024-11-12 11:25:52 +0500

    Реализован функционал добавления в app.py Пункт 2.1 завершен

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import datetime
+from typing import Annotated
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 class Order(BaseModel):
     number : int
@@ -29,3 +30,7 @@ app = FastAPI()
 @app.get("/orders")
 def get_orders():
     return repo
+
+@app.post("/orders")
+def create_order(dto : Annotated[Order , Form()]):
+    repo.append(dto)
